4a09f75f277314a2dbd46a6f6f57e57c5cf4184d
ysub23y <127386235+ysub23y@users.noreply.github.com>
2023-03-22 09:31:26 +0900

    Add files via upload
    백준 5543번 문제 코드입니다.

---
+++ bj5543.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+	int b, d;
+	int L = 0, I = 0;
+	for (int i = 0; i < 3; i++) {
+		scanf_s("%d", &b);
+		if (L == 0) L = b;
+		if (L > b) L = b;
+
+	}
+	for (int i = 0; i < 2; i++) {
+		scanf_s("%d", &d);
+		if (I == 0) I = d;
+		if (I > d) I = d;
+	}
+	printf("%d", L + I - 50);
+	return 0;
+}
